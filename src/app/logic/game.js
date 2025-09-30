34c8e06499fdb54d0901961e969eb74087b7f091
José Manuel Pérez Rodríguez <josemanuelpr@ufps.edu.co>
2025-09-30 14:26:03 -0500

    Structuring the project

---
+++ src/app/logic/game.js
@@ -0,0 +1,38 @@
+class Game {
+    constructor(){
+        this.board = Array(9).fill(null)
+        this.xIsNext = true
+    }
+
+    get currentPlayer(){
+        return xIsNext ? "X" : "O"
+    }
+
+    set board(squares){
+        this.board = squares
+    }
+
+
+
+    calculateWinner() {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i]
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a]
+            }
+        }
+
+        return null
+    }
+}
